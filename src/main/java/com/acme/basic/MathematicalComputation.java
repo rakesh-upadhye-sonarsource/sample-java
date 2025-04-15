2765141d2c8c1a0554494941481673b0bd7be202
Rakesh Upadhye <77496849+rakesh-upadhye-sonarsource@users.noreply.github.com>
2025-04-15 14:55:46 +0200

    Update MathematicalComputation.java to corrcet addition method and add subtraction method

--- src/main/java/com/acme/basic/MathematicalComputation.java
+++ src/main/java/com/acme/basic/MathematicalComputation.java
@@ -4,6 +4,11 @@ public class MathematicalComputation.java {
 
   int add(int a , int b) {
     System.out.println("Addition is : " a+b);
-    return a=b;
+    return a+b;
   }
+  int sub(int a , int b) {
+    System.out.println("Subtraction is : " a-b);
+    return a-b;
+  }
+  
 }
